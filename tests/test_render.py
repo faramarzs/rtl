052aff05ab38b6801e9943be18cbf35778081485
Faramarz Saberi <f.saberi@adpdigital.com>
2023-03-07 13:54:51 +0330

    update tests.

--- tests/test_render.py
+++ tests/test_render.py
@@ -5,6 +5,7 @@ from rtl.model import RTLAlgorithm, Direction
 
 RTL_TEXT = "روباه"
 RTL_WITH_SPACE_TEXT = "شیر قهوه‌ای"
+RTL_WITH_SPACE_TEXT2 = "ه‌ا"
 RTL_NUMBER = "۴۹۶"
 RTL_MARKS1 = "؟"
 RTL_MARKS2 = "،"
@@ -22,9 +23,10 @@ def test_render():
     strs = [
         #RTL_TEXT + RTL_NUMBER + RTL_WITH_SPACE_TEXT + RTL_MARKS1,
         #LTR_TEXT + NEUTRAL_TEXT + LTR_WITH_SPACE_TEXT + LTR_MARKS1,
-        LTR_TEXT + NEUTRAL_TEXT + RTL_WITH_SPACE_TEXT + NEUTRAL_TEXT + LTR_WITH_SPACE_TEXT + LTR_MARKS2,
+        #LTR_TEXT + NEUTRAL_TEXT + RTL_WITH_SPACE_TEXT + NEUTRAL_TEXT + LTR_WITH_SPACE_TEXT + LTR_MARKS2,
         #RTL_TEXT,
         #RTL_WITH_SPACE_TEXT,
+        "۱۲ عید نوروز"
     ]
     with open("/tmp/x.txt", "w") as f:
         for s in strs:
@@ -32,4 +34,6 @@ def test_render():
             print('', file=f)
             s2 = analyzer.analyze(s, direction=Direction.RTL, algo=RTLAlgorithm.DIR_BASED)
             print(s2, file=f)
+            sl = list(s)
+            sl2 = list(s2)
             print('-----------------------------------------------------------------------', file=f)
